dc6d67f8bf22d839a76ab734b1cc4b46f39c811e
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-10-05 19:20:35 +0200

    Running in a Docker Container

--- src/settings.py
+++ src/settings.py
@@ -99,22 +99,30 @@ WSGI_APPLICATION = 'wsgi.application'
 #     }
 # }
 
+# For development
 DATABASES = {
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "pymonit",
-        # Not used with sqlite3.
-        "USER": "pymonit",
-        # Not used with sqlite3.
-        "PASSWORD": "yourpassword",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "localhost",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'data/monitcollector.sqlite3',
     }
 }
+
+#DATABASES = {
+#    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+#        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+#        "NAME": "pymonit",
+        # Not used with sqlite3.
+#        "USER": "pymonit",
+        # Not used with sqlite3.
+#        "PASSWORD": "yourpassword",
+        # Set to empty string for localhost. Not used with sqlite3.
+#        "HOST": "localhost",
+        # Set to empty string for default. Not used with sqlite3.
+#        "PORT": "",
+#    }
+#}
 
 
 # Internationalization
