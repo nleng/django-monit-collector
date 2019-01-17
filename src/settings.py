8b3db7acc9c3bba77811dd317ea8a318f15a2525
Nicor Lengert <nicorlengert@gmail.com>
2019-01-17 09:42:02 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -82,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'staticfiles' : 'django.templatetags.static',
+                'monitcollector_tags' : 'monitcollector.templatetags.monitcollector_tags',
+            }
         },
     },
 ]
